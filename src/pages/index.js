fb04edf7f5d726b43cb0b21994de2c3443e7557e
shimmiChristo <shimmin.christopher@gmail.com>
2019-06-12 19:26:03 -0400

    create a gatsby website and practicing with React!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
+import Header from "../components/header"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ color: `purple` }}>
+    <h1>Hello Gatsby!</h1>
+    <Header headerText="About Gatsbys" /> 
+    <Header headerText="It's pretty cool" /> 
+    <p>What a world.</p>
+  </div>
+)
