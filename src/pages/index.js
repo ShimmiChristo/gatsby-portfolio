e97daa551cd336a49acad09337aabe285375e26e
shimmiChristo <shimmin.christopher@gmail.com>
2019-06-24 06:26:45 -0400

    Created pages with data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link, graphql } from "gatsby"
 // import { css } from "@emotion/core"
 // import { rhythm } from "../utils/typography"
 import Layout from "../components/layout"
@@ -20,17 +20,22 @@ export default ({ data }) => {
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
+            <Link 
+              to={node.fields.slug}
+            >
+
             <h3>
               {node.frontmatter.title}{" "}
               <span
                 // css={css`
                 //   color: #bbb;
                 // `}
-              >
+                >
                 — {node.frontmatter.date}
               </span>
             </h3>
             <p>{node.excerpt}</p>
+            </Link>
           </div>
         ))}
       </div>
@@ -48,6 +53,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
